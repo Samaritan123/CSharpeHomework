29cb6b13501fac66a41ab8482afe065a40dcae23
Samaritan123 <47918006+Samaritan123@users.noreply.github.com>
2019-02-28 21:43:14 +0800

    Add files via upload

---
+++ homework1/1.4/ConsoleApp1/a+b.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + b);
+        }
+    }
+}
---
+++ homework1/1.5/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox2.Text;
+            int a = int.Parse(s1), b = int.Parse(s2);
+            int c = a * b;
+            label1.Text = c + "";
+        }
+    }
+}
