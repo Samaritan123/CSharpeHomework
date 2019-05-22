c5b000301bf9fb42ffa976e12490b4c9949ec7e8
Samaritan123 <47918006+Samaritan123@users.noreply.github.com>
2019-05-22 19:49:04 +0800

    Add files via upload

---
+++ homework3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public abstract class CellPhone
+    {
+        public abstract void Print();
+    }
+
+    public class HuaWei: CellPhone
+    {
+        public override void Print()
+        {
+            Console.WriteLine("生产了一台华为手机！");
+        }
+    }
+
+    public class Iphone: CellPhone
+    {
+        public override void Print()
+        {
+            Console.WriteLine("生产了一台苹果手机！");
+        }
+    }
+
+    public class XiaoMi: CellPhone
+    {
+        public override void Print()
+        {
+            Console.WriteLine("生产了一台小米手机!");
+        }
+    }
+
+    public class PhoneFactory
+    {
+        private CellPhone phone = null;
+        public CellPhone CreatePhone(string brand)
+        {
+            switch (brand)
+            {
+                case "HuaWei":
+                    phone = new HuaWei();
+                    break;
+                case "Iphone":
+                    phone = new Iphone();
+                    break;
+                case "XiaoMi":
+                    phone = new XiaoMi();
+                    break;
+                default:
+                    break;
+            }
+            return phone;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneFactory factory = new PhoneFactory();
+            CellPhone phone1 = factory.CreatePhone("HuaWei");
+            CellPhone phone2 = factory.CreatePhone("Iphone");
+            CellPhone phone3 = factory.CreatePhone("XiaoMi");
+            phone1.Print();
+            phone2.Print();
+            phone3.Print();
+        }
+    }
+}
---
+++ homework3/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class SingletonMode
+    {
+        public static SingletonMode instance { get; private set; } = null;
+        private SingletonMode()
+        {
+            Console.WriteLine("构造函数");
+        }
+
+        static SingletonMode()///Clr运行的时候，进行初始化，并且只执行一次
+        {
+            instance = new SingletonMode();
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Hello World！！");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonMode.instance.Print();
+        }
+    }
+}
